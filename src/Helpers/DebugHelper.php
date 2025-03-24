e0139f3dc7fdca6d6d031742650dba8237f6119e
DanMDNexus <dan.baker@md-nexus.com>
2025-03-24 21:17:36 +0000

    Added method for executionTime

--- src/Helpers/DebugHelper.php
+++ src/Helpers/DebugHelper.php
@@ -38,4 +38,21 @@ class DebugHelper
 
         return sprintf('%.2f %s', $bytes / (1024 ** $factor), $units[$factor]);
     }
+
+    /**
+     * Measure execution time of a callable in milliseconds.
+     *
+     * @param callable $callback
+     * @return float Execution time in milliseconds.
+     */
+    public static function executionTime(callable $callback): float
+    {
+        $startTime = microtime(true);
+
+        $callback();
+
+        $endTime = microtime(true);
+
+        return round(($endTime - $startTime) * 1000, 4);
+    }
 }

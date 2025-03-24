60155a90c697fdfb53292e71e79a4be6817b0123
DanMDNexus <dan.baker@md-nexus.com>
2025-03-24 21:10:45 +0000

    Added test for toCamelCase

--- tests/Helpers/StringHelperTest.php
+++ tests/Helpers/StringHelperTest.php
@@ -32,4 +32,30 @@ class StringHelperTest extends TestCase
         $this->assertEquals('hello_world', StringHelper::toSnakeCase('hello-world'));
         $this->assertEquals('test_string', StringHelper::toSnakeCase('test__string'));
     }
+
+    /** @test */
+    public function converts_snake_case_to_camel_case()
+    {
+        $this->assertEquals('myVariableName', StringHelper::toCamelCase('my_variable_name'));
+    }
+
+    /** @test */
+    public function converts_kebab_case_to_camel_case()
+    {
+        $this->assertEquals('myVariableName', StringHelper::toCamelCase('my-variable-name'));
+    }
+
+    /** @test */
+    public function converts_spaces_to_camel_case()
+    {
+        $this->assertEquals('myVariableName', StringHelper::toCamelCase('my variable name'));
+    }
+
+    /** @test */
+    public function handles_mixed_and_special_characters()
+    {
+        $this->assertEquals('helloWorld123', StringHelper::toCamelCase('Hello World 123'));
+        $this->assertEquals('testString', StringHelper::toCamelCase('test__string'));
+        $this->assertEquals('myVariable123Name', StringHelper::toCamelCase('MyVariable_123-name'));
+    }
 }

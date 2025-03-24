d0b949f2bfc90d97cf0e4b8e382a96c81ef812fa
DanMDNexus <dan.baker@md-nexus.com>
2025-03-24 20:38:57 +0000

    Added utility to create UUID

---
+++ src/Utilities/Uuid.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace DanBaker\ToolBox\Utilities;
+
+class Uuid
+{
+    /**
+     * Generate a UUID v4.
+     *
+     * @return string
+     */
+    public static function v4(): string
+    {
+        $data = random_bytes(16);
+
+        // Set version to 0100 (UUID v4)
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+
+        // Set variant to 10xx
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
+}

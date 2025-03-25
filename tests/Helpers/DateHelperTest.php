59d7f87e1a6f424fc7af7ecdc6d8337eafde23a3
DanMDNexus <dan.baker@md-nexus.com>
2025-03-25 19:23:52 +0000

    Added nextWeekday method and test

--- tests/Helpers/DateHelperTest.php
+++ tests/Helpers/DateHelperTest.php
@@ -31,4 +31,29 @@ class DateHelperTest extends TestCase
         $previous = DateHelper::previousWeekday('2025-03-24');
         $this->assertEquals('2025-03-21', $previous->format('Y-m-d'));
     }
+
+    /** @test */
+    public function gets_next_weekday_from_weekday()
+    {
+        $date = new \DateTimeImmutable('2025-03-25'); // Tuesday
+        $next = DateHelper::nextWeekday($date);
+
+        $this->assertEquals('2025-03-26', $next->format('Y-m-d')); // Wednesday
+    }
+
+    /** @test */
+    public function skips_weekend_from_friday()
+    {
+        $date = new \DateTimeImmutable('2025-03-21'); // Friday
+        $next = DateHelper::nextWeekday($date);
+
+        $this->assertEquals('2025-03-24', $next->format('Y-m-d')); // Monday
+    }
+
+    /** @test */
+    public function handles_string_input_for_next_weekday()
+    {
+        $next = DateHelper::nextWeekday('2025-03-21');
+        $this->assertEquals('2025-03-24', $next->format('Y-m-d'));
+    }
 }

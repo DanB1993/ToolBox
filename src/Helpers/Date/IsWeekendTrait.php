b54d500ca6eb25a4eff0e832dc78b2077f4fdca0
Dan Baker <boyciebsson@gmail.com>
2025-03-29 21:30:43 +0000

    Refactored DateHelper code

---
+++ src/Helpers/Date/IsWeekendTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace DanBaker\ToolBox\Helpers\Date;
+
+trait IsWeekendTrait
+{
+    /**
+     * Check if a given date falls on a weekend (Saturday or Sunday).
+     *
+     * @param \DateTimeInterface|string|null $date
+     * @return bool
+     */
+    public static function isWeekend(\DateTimeInterface|string|null $date = null): bool
+    {
+        $date = $date instanceof \DateTimeInterface
+            ? $date
+            : new \DateTimeImmutable($date ?? 'now');
+
+        $dayOfWeek = (int) $date->format('N'); // 6 = Saturday, 7 = Sunday
+
+        return $dayOfWeek >= 6;
+    }
+}
